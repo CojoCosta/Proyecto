707d7bce9afcbbe8dc2c5d1cb74b1c05756e7192
CojoCosta <diegocostapereira18@gmail.com>
2026-01-27 16:40:13 +0100

    changes 27/01

---
+++ App_Web/vistas/Header.php
@@ -0,0 +1,17 @@
+<style>
+    .header{
+        width: 100%;
+        height: 15%;
+        background-color: blue;
+        border-bottom: solid;
+        border-color: white;
+    }
+    
+</style>
+
+<div id = "header">
+    <div id = "logo">
+        <img src="App_Web\recursos\logoTransparent.png">
+        
+    </div>
+</div>
