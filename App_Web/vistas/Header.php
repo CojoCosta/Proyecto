ca572f6e9956454f5beb68a17e63ea917308a23c
CojoCosta <diegocostapereira18@gmail.com>
2026-01-27 16:44:56 +0100

    changes 27/01

--- App_Web/vistas/Header.php
+++ App_Web/vistas/Header.php
@@ -1,17 +1,42 @@
 <style>
-    .header{
-        width: 100%;
-        height: 15%;
-        background-color: blue;
-        border-bottom: solid;
-        border-color: white;
+    /* Reset básico */
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
     }
-    
-</style>
 
+    /* Header principal */
+    #header {
+        width: 100%;
+        height: 70px;
+        background-color: #0a1f44; /* Azul oscuro */
+        display: flex;
+        align-items: center;
+        padding: 0 20px;
+        box-shadow: 0 2px 6px rgba(0, 0, 0, 0.3);
+    }
+
+    /* Contenedor del logo */
+    #logo {
+        display: flex;
+        align-items: center;
+    }
+
+    /* Imagen del logo */
+    #logo img {
+        height: 45px;
+        width: auto;
+        cursor: pointer;
+    }
+
+    /* Efecto hover sutil */
+    #logo img:hover {
+        opacity: 0.9;
+    }
+</style>
 <div id = "header">
     <div id = "logo">
         <img src="App_Web\recursos\logoTransparent.png">
-        
     </div>
 </div>
