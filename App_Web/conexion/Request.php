2e1c6b8121e5454d021f65ec773fe85507828e1a
CojoCosta <dcosta1882000@gmail.com>
2026-02-03 13:13:04 +0100

    changes03/02

---
+++ App_Web/conexion/Request.php
@@ -0,0 +1,36 @@
+<?php
+
+class Request
+{
+    private $basePath;
+
+    public function __construct($basePath)
+    {
+        $this->basePath = $basePath;
+    }
+
+    public function request($method, $url, $data = null)
+    {
+        $cURLHandle = curl_init($this->basePath . $url);
+
+        curl_setopt($cURLHandle, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($cURLHandle, CURLOPT_CUSTOMREQUEST, $method);
+
+        if ($data !== null) {
+            curl_setopt($cURLHandle, CURLOPT_HTTPHEADER, [
+                'Content-Type: application/json'
+            ]);
+            curl_setopt($cURLHandle, CURLOPT_POSTFIELDS, json_encode($data));
+        }
+
+        $response = curl_exec($cURLHandle);
+
+        if (curl_errno($cURLHandle)) {
+            throw new Exception("Error en la API: " . curl_error($cURLHandle));
+        }
+
+        curl_close($cURLHandle);
+
+        return json_decode($response);
+    }
+}
--- App_Web/modelos/Usuario.php
+++ App_Web/modelos/Usuario.php
@@ -1,92 +1,89 @@
 <?php
+require_once 'Request.php';
 
-Class Usuario{
+class Usuario
+{
     private $path;
-    private $usuario;
+    private $request;
+    private $nombre_usuario;
     private $nombre;
     private $apellidos;
-    private $nombreUsuario;
     private $email;
     private $password;
     private $fechaNacimiento;
-    private $database;
 
-    public function __construct(){
-        $this->path = "http://10.0.2.2:8080/apirest/rest/usuario/";
-        $this->usuario = array();
+    public function __construct()
+    {
+        $this->path = "http://10.0.2.2:8080/apirest/rest/usuario";
+        $this->request = new Request($this->path);
+        $this->nombre_usuario = "";
         $this->nombre = "";
         $this->apellidos = "";
-        $this->nombreUsuario = "";
         $this->email = "";
         $this->password = "";
         $this->fechaNacimiento = "";
-        $this->database = Conexion::conexion();
-    }
-    #region SET Y GET
-    public function setNombre($nombre){
-        $this->nombre = $nombre;
-    }
-    public function getNombre(){
-        return $this->nombre;
     }
 
-    public function setApellidos($apellidos){
-        $this->apellidos = $apellidos;
-    }
-    public function getApellidos(){
-        return $this->apellidos;
-    }
+    #region SETTERS Y GETTERS
+    public function setNombreUsuario($nombre_usuario) { $this->nombre_usuario = $nombre_usuario; }
+    public function getNombreUsuario() { return $this->nombre_usuario; }
 
-    public function setNombreUsuario($nombreUsuario){
-        $this->nombreUsuario = $nombreUsuario;
-    }
-    public function getNombreUsuario(){
-        return $this->nombreUsuario;
-    }
+    public function setNombre($nombre) { $this->nombre = $nombre; }
+    public function getNombre() { return $this->nombre; }
 
-    public function setEmail($email){
-        $this->email = $email;
-    }
-    public function getEmail(){
-        return $this->email;
-    }
+    public function setApellidos($apellidos) { $this->apellidos = $apellidos; }
+    public function getApellidos() { return $this->apellidos; }
 
-    public function setPassword($password){
-        $this->password = $password;
-    }
-    public function getPassword(){
-        return $this->password;
-    }
+    public function setEmail($email) { $this->email = $email; }
+    public function getEmail() { return $this->email; }
 
-    public function setfechaNacimiento($fechaNacimiento){
-        $this->fechaNacimiento = $fechaNacimiento;
-    }
-    public function getFechaNacimineto(){
-        return $this->fechaNacimiento;
-    }
+    public function setPassword($password) { $this->password = $password; }
+    public function getPassword() { return $this->password; }
+
+    public function setFechaNacimiento($fechaNacimiento) { $this->fechaNacimiento = $fechaNacimiento; }
+    public function getFechaNacimiento() { return $this->fechaNacimiento; }
     #endregion
 
-    public function setUsuario($nombre, $apellidos, $nombreUsuario, $email, $password, $fechaNacimiento){
-        $sql = "INSERT INTO usuarios (nombre, apellidos, nombreUsuario, email, password, fechaNacimiento) VALUES ('$nombre', '$apellidos', '$nombreUsuario', '$email', '$password', '$fechaNacimiento')";
-        $result = $this->database->query($sql);
-        $this -> database = null;
-        return $result;
-    }
-        public function getUsuario(){
-        $sql = "SELECT * FROM usuarios";
-        $result = $this -> database -> query($sql);
-        $this -> usuario = $result -> fetchAll(PDO:: FETCH_ASSOC);
-        $this -> database = null;
-        return $this -> usuario;
+    public function getUsuario()
+    {
+        return $this->request->request('GET', '/usuarios');
     }
 
-    public function borrarUsuario($nombreUsuario){
-        $sql = "DELETE FROM usuarios WHERE nombreUsuario LIKE '$nombreUsuario'";
-        $result = $this->database->query($sql);
-        $this->database = null;
-        return $result;
+    public function registrarUsuario($nombre_usuario, $nombre, $apellidos, $email, $password, $fecha_nacimiento)
+    {
+        $data = [
+            'nombre_usuario' => $nombre_usuario,
+            'nombre' => $nombre,
+            'apellidos' => $apellidos,
+            'email' => $email,
+            'password' => $password,
+            'fecha_nacimiento' => $fecha_nacimiento
+        ];
+
+        return $this->request->request('POST', '/usuarios', $data);
     }
 
+    public function editarUsuario($id)
+    {
+        return $this->request->request('GET', "/usuarios/{$id}");
+    }
 
+    public function actualizarUsuario($id, $nombre_usuario, $nombre, $apellidos, $email, $password, $fecha_nacimiento)
+    {
+        $data = [
+            'nombre_usuario' => $nombre_usuario,
+            'nombre' => $nombre,
+            'apellidos' => $apellidos,
+            'email' => $email,
+            'password' => $password,
+            'fecha_nacimiento' => $fecha_nacimiento
+        ];
 
+        return $this->request->request('PUT', "/usuarios/{$id}", $data);
+    }
+
+    public function borrarUsuario($id)
+    {
+        return $this->request->request('DELETE', "/usuarios/{$id}");
+    }
 }
